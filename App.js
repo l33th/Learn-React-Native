0589f3e4a4e3890e24b47e14e5b2afdc7c65de9f
l33th <l33th@protonmail.ch>
2021-11-17 08:05:15 +0200

    on press handler function

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('Text pressed');
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}>My first native app! adkljfldfjdhgljkdhkldsjsdjf;ojd;of;osdjf;lsdjf;sjdfoisjdfohdhfhfiojeojha;ojrioj;ojfz </Text>
+      <Text numberOfLines={1} onPress={handlePress}>My first native app! adkljfldfjdhgljkdhkldsjsdjf;ojd;of;osdjf;lsdjf;sjdfoisjdfohdhfhfiojeojha;ojrioj;ojfz </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
