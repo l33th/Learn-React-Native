a691d23e9ac1b27c8e8348f36844ef27c3f565b3
l33th <l33th@protonmail.ch>
2021-11-17 07:26:40 +0200

    testing errors

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   // this console log is just for during development
-  console.log('App Executed.')
+  console.log('App Executed.');
+  let x;
+  x.toString();
   return (
     <View style={styles.container}>
       <Text>My first native app!</Text>
