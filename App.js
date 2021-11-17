c4139ee22df48491f0642ab6b009a84d62c0cf83
l33th <l33th@protonmail.ch>
2021-11-17 08:27:54 +0200

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed');
@@ -8,13 +8,15 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Generate Random Image</Text>
-      <Image 
-      blurRadius={1} 
-      fadeDuration={1000}
-      source={{ 
-        width: 200,
-        height: 300,
-        uri: 'https://picsum.photos/200/300'}} />
+      <TouchableOpacity onPress={() => console.log('image tapped')}>
+        <Image 
+        blurRadius={1} 
+        fadeDuration={1000}
+        source={{ 
+          width: 200,
+          height: 300,
+          uri: 'https://picsum.photos/200/300'}} />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
