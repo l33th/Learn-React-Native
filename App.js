16768ae5626f82e8180aa7b5119c6c7f5bb0ec88
l33th <l33th@protonmail.ch>
2021-11-17 08:13:15 +0200

    images

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed');
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>My first native app! adkljfldfjdhgljkdhkldsjsdjf;ojd;of;osdjf;lsdjf;sjdfoisjdfohdhfhfiojeojha;ojrioj;ojfz </Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={handlePress}>My first native app!</Text>
+      <Image source={require('./assets/icon.png')} />
     </SafeAreaView>
   );
 }
@@ -15,7 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
-    
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
