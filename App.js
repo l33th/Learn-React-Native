8ba4454c5a1981645501609f592bac8df7560873
l33th <l33th@protonmail.ch>
2021-11-17 08:29:23 +0200

    TouchableHighlight

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed');
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Generate Random Image</Text>
-      <TouchableOpacity onPress={() => console.log('image tapped')}>
+      <TouchableHighlight onPress={() => console.log('image tapped')}>
         <Image 
         blurRadius={1} 
         fadeDuration={1000}
@@ -16,7 +16,7 @@ export default function App() {
           width: 200,
           height: 300,
           uri: 'https://picsum.photos/200/300'}} />
-      </TouchableOpacity>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
