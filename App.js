7a606d7b447a97264ac7eb7160376f6c75c97a45
l33th <l33th@protonmail.ch>
2021-11-17 07:59:41 +0200

    views - SafeAreaView

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  let x = 1;
-  // this console log is just for during development
-  console.log('App Executed.');
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>My first native app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    
   },
 });
