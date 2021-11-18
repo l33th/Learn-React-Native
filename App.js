04d7af0d959500d7dd47191d8951358d789352b4
l33th <l33th@protonmail.ch>
2021-11-18 07:56:03 +0200

    flexbox

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { Dimensions, StyleSheet, SafeAreaView, Button, Platform, StatusBar, View } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { View } from 'react-native';
 
 export default function App() {
-  const buttonPressed = () => console.log('button pressed');
-  const {landscape} =  useDeviceOrientation();
+
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Button color='purple' title='Click me' onPress={buttonPressed} />
-      <View style={{backgroundColor: 'dodgerblue', width: '100%', height: landscape ? '100%' : '30%',}}></View>
-    </SafeAreaView>
+    <View style={{backgroundColor: '#fff', flex: 1,}}>
+      <View style={{backgroundColor: 'dodgerblue', flex: 1}} />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-});
