0528b5c4696bd27e9eb9345dc6e3981a72362349
l33th <l33th@protonmail.ch>
2021-11-18 07:56:58 +0200

    divided into three color segments

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <View style={{backgroundColor: '#fff', flex: 1,}}>
       <View style={{backgroundColor: 'dodgerblue', flex: 1}} />
+      <View style={{backgroundColor: 'gold', flex: 1}} />
+      <View style={{backgroundColor: 'tomato', flex: 1}} />
     </View>
   );
 }
