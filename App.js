9491018250aea8e0608d000226f0a9c2723e64db
l33th <l33th@protonmail.ch>
2021-11-18 06:52:29 +0200

    button setup

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableNativeFeedback, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
-  const handleTextPress = () => console.log('Text pressed');
-  const handleImagePress = () => console.log('image tapped');
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handleTextPress}>Generate Random Image</Text>
-      <TouchableNativeFeedback onPress={handleImagePress}>
-        <Image 
-        blurRadius={1} 
-        fadeDuration={1000}
-        source={{ 
-          width: 200,
-          height: 300,
-          uri: 'https://picsum.photos/200/300'}} />
-          <View style={{width: 200, height: 70, backgroundColor: 'dodgerblue'}}></View>
-      </TouchableNativeFeedback>
+      <Button title='Click me' />
     </SafeAreaView>
   );
 }
