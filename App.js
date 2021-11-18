908af203f753546aa2a9e47351fea19234b9251d
l33th <l33th@protonmail.ch>
2021-11-18 07:34:00 +0200

    scaling, getting dimensions and styling

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView, Button, Alert } from 'react-native';
+import { Dimensions, StyleSheet, SafeAreaView, Button, Platform, StatusBar, View } from 'react-native';
 
 export default function App() {
-  const buttonPressed = () => Alert.prompt('My title', 'My message', (text) => console.log(text));
+  const buttonPressed = () => console.log('button pressed');
+  console.log(Dimensions.get('screen'))
 
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
+    <SafeAreaView style={styles.container}>
       <Button color='purple' title='Click me' onPress={buttonPressed} />
+      <View style={{backgroundColor: 'dodgerblue', width: '50%', height: 70}}></View>
     </SafeAreaView>
   );
 }
-
-const containerStyle = { backgroundColor: 'orange'}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
