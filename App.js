aed13aac63a3e2e1aee4cccc700a7a753772a1c4
l33th <l33th@protonmail.ch>
2021-11-18 08:07:22 +0200

    more flex to dodgerblue

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { View } from 'react-native';
 
 export default function App() {
 
-
   return (
     <View style={{backgroundColor: '#fff', flex: 1,}}>
       <View style={{backgroundColor: 'dodgerblue', flex: 2}} />
