b9c4027caf4b3cc33f467b30e2a5a344c0873c18
l33th <l33th@protonmail.ch>
2021-11-19 12:16:35 +0200

    absolute positioning

--- App.js
+++ App.js
@@ -26,10 +26,11 @@ export default function App() {
           backgroundColor: 'gold',
           width: 100,
           height: 100,
-          top: -20,
+          top: 20,
           // bottom: -20,
-          // left: -20,
+          left: 20,
           // right: -20,
+          position: 'absolute',
         }}
       />
       <View
