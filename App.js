65b7fea5eebdde4a946efeae87cb3c43fcfe302a
l33th <l33th@protonmail.ch>
2021-11-19 12:01:33 +0200

    alignment test - blue item

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
         style={{
           backgroundColor: 'dodgerblue',
           width: 100,
-          height: 100,
+          height: 300,
         }}
       />
       <View style={{ backgroundColor: 'gold', width: 100, height: 100 }} />
