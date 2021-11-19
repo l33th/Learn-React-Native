55334bdf4da01788001724b3794919ca197fef0d
l33th <l33th@protonmail.ch>
2021-11-19 11:18:37 +0200

    flex direction

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 export default function App() {
-
   return (
-    <View style={{backgroundColor: '#fff', flex: 1,}}>
-      <View style={{backgroundColor: 'dodgerblue', flex: 2}} />
-      <View style={{backgroundColor: 'gold', flex: 1}} />
-      <View style={{backgroundColor: 'tomato', flex: 1}} />
+    <View style={{ backgroundColor: '#fff', flex: 1 }}>
+      <View
+        style={{ backgroundColor: 'dodgerblue', width: 100, height: 100 }}
+      />
+      <View style={{ backgroundColor: 'gold', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'tomato', width: 100, height: 100 }} />
     </View>
   );
 }
-
