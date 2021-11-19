59d5d538a49b892d44875ab11e92848e22ba6998
l33th <l33th@protonmail.ch>
2021-11-19 11:20:50 +0200

    flex direction row-reverse

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: '#fff', flex: 1, flexDirection: 'row' }}>
+    <View
+      style={{ backgroundColor: '#fff', flex: 1, flexDirection: 'row-reverse' }}
+    >
       <View
         style={{ backgroundColor: 'dodgerblue', width: 100, height: 100 }}
       />
